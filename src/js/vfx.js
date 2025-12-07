adf4edb706c780e8b26a4524ee965a2d3b6ce9a0
nicolascruz <2363567@cmontmorency.qc.ca>
2025-12-07 12:27:59 -0500

    intialisation code de base et quelques medias

---
+++ src/js/vfx.js
@@ -0,0 +1,9 @@
+//const player = new Tone.Player({
+//	url: "https://github.com/PerformX2/cruz_nicolas_web3tp2/tree/main/",
+//	loop: true,
+//	autostart: true,
+//});
+//
+//const distortion = new Tone.Distortion(0.4).toDestination();
+//
+//player.connect(distortion);
